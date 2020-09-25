45776452b9a8b1522256f8752f120884dac2e307
Léo Freitas <leonardo.eng3965@gmail.com>
2020-09-25 11:45:05 -0300

    App completo

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,110 @@
+import 'package:first_flutter_app/utils/componentes.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
+void main() {
+  runApp(MyApp());
+}
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+
+  TextEditingController txtPeso   = TextEditingController();
+  TextEditingController txtAltura = TextEditingController();
+  GlobalKey<FormState> cForm = GlobalKey<FormState>();
+  String info = "Informe os seus dados";
+
+  Function validaPeso = ((value){
+    if(value.isEmpty){
+      return "Informe o Peso";
+    }
+    if(int.parse(value) <=0){
+      return "O peso não deve ser zero ou negativo";
+    }
+    return null;
+  });
+
+  Function validaAltura = ((value){
+    if(value.isEmpty){
+      return "Informe a Altura";
+    }
+    if(double.parse(value) <=0){
+      return "A altura não deve ser zero ou negativo";
+    }
+    return null;
+  });
+
+  resetaForm(){
+    txtPeso.text="";
+    txtAltura.text="";
+    setState(() {
+      info = "Informe os seus dados";
+      cForm = GlobalKey<FormState>();
+    });
+
+  }
+
+  calcularImc(){
+    setState(() {
+      if(!cForm.currentState.validate()){
+        return null;
+      }
+      double p = double.parse(txtPeso.text);
+      double a = double.parse(txtAltura.text);
+      double imc = p/(a*a);
+      if(imc < 18.5)
+        info = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
+      else
+      if(imc >= 18.5 && imc < 30)
+        info = "Peso ideal (${imc.toStringAsPrecision(4)})";
+      else
+        info = "Acima do peso (${imc.toStringAsPrecision(4)})";
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: paginaPrincipal(),
+      home: telaPrincipal(),
     );
   }
 
-  paginaPrincipal() {
+  telaPrincipal() {
     return Scaffold(
-      appBar: barraPrincipal(),
-      body : Container(
-        child: textoPrincipal(),
+      appBar: AppBar(
+        title: Text("IMC Flutter"),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: resetaForm,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: clicouActionButton,
-        child: Icon(Icons.add),
+      body: Form(
+        key: cForm,
+        child: Container(
+          padding: EdgeInsets.fromLTRB(40, 0, 40, 0),
+          child: Column(
+
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Componentes.caixaDeTexto("Peso", "Digite o Peso", txtPeso, validaPeso, numero: true),
+              Componentes.caixaDeTexto("Altura", "Digite a Altura", txtAltura, validaAltura, numero: true),
+              Componentes.botao("Calcular", calcularImc),
+              Text(
+                info,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
-
-  clicouActionButton(){
-    print(" Clicou ");
-  }
-
-  barraPrincipal() {
-    return AppBar(
-      title: Text("Hello World"),
-      centerTitle: true,
-      backgroundColor: Colors.grey,
-    );
-  }
-
-  textoPrincipal() {
-    return Text(
-      "Goodbye World",
-      style: estiloTexto(40, true, Colors.red)
-    );
-  }
-}
-
-estiloTexto(var tam, bool negrito, Color cor){
-  return TextStyle(
-    color: cor,
-    fontWeight: negrito?FontWeight.bold:FontWeight.normal,
-    fontSize: tam,
-  );
 }
